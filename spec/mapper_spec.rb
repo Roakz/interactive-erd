67bb1c8b822a555e8729348de156e65f1570b0ff
Roarkz <rory_bell_88@hotmail.com>
2020-08-23 20:04:59 +1000

    calculate top level spec

--- spec/mapper_spec.rb
+++ spec/mapper_spec.rb
@@ -1,7 +1,18 @@
 require_relative '../mapper'
 
 RSpec.describe Mapper do
+
+before(:all) do
+ @mapper = Mapper.new(:file => File.open("test.sql"))
+end
+
   it "performs a hello world test" do
     expect("hello world!").to eq("hello world!")
   end
+
+  it "returns the databases top level names" do
+    top_level_names = @mapper.calculate_top_level
+    expect(top_level_names).to eq({:database => [], :schema => ["production", "sales"]})
+  end
+  
 end
