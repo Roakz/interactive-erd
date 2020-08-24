056e495dff81acd28d64e977348770383f868b7c
Roarkz <rory_bell_88@hotmail.com>
2020-08-24 20:31:53 +1000

    added tests for entities to json function

--- spec/mapper_spec.rb
+++ spec/mapper_spec.rb
@@ -1,4 +1,5 @@
 require_relative '../mapper'
+require_relative 'column_list'
 
 # This test file uses test.sql and loads it into the Mapper class to use a set of known data to test results.
 
@@ -7,7 +8,7 @@ RSpec.describe Mapper do
 before(:all) do
  @mapper = Mapper.new(:file => File.open("test.sql"))
 end
-  context("calculate top level") do
+  context("calculate top level function") do
 
     it "performs a hello world test" do
       expect("hello world!").to eq("hello world!")
@@ -19,23 +20,68 @@ end
     end
   end
 
-  context("split entities") do 
+  context("split entities function") do 
 
     before(:all) do
       @result = @mapper.split_entities
     end
 
-    it "returns an array" do
+    it "should return an array" do
       expect(@result).to be_a(Array)
     end 
 
-    it "contains the correct amount of entities" do
+    it "should contain all entities" do
       expect(@result.length).to eq(9)
     end
 
-    it "returns entities correctly as expected" do 
+    it "should return the entities as expected" do 
       expect(@result[0]).to eq("CREATE TABLE production.categories (\r\n\tcategory_id INT IDENTITY (1, 1) PRIMARY KEY,\r\n\tcategory_name VARCHAR (255) NOT NULL\r\n);\r\n")
       expect(@result[8]).to eq("CREATE TABLE production.stocks (\r\n\tstore_id INT,\r\n\tproduct_id INT,\r\n\tquantity INT,\r\n\tPRIMARY KEY (store_id, product_id),\r\n\tFOREIGN KEY (store_id) REFERENCES sales.stores (store_id) ON DELETE CASCADE ON UPDATE CASCADE,\r\n\tFOREIGN KEY (product_id) REFERENCES production.products (product_id) ON DELETE CASCADE ON UPDATE CASCADE\r\n);")
     end
+  end
+  
+  context("entites to json function") do
+    
+    before(:all) do
+      @entities = @mapper.split_entities
+      @result = @mapper.entities_to_json(@entities)
+    end
+
+    it "should be a valid json object" do
+    end
+
+    it "should contain the top level objects" do
+      expect(@result).to include("top_level")
+      expect(@result["top_level"]).to eq({:database => [], :schema => ["production", "sales"]})
+    end
+
+    it "should contain all entities" do
+      expect(@result).to include("entities")
+      @returned_array = []
+      @result["entities"].each {|obj| @returned_array << obj[:table_name]}
+      expect(@returned_array).to contain_exactly(
+        "production.categories",
+        "production.brands",
+        "production.products",
+        "sales.customers",
+        "sales.stores",
+        "sales.staffs",
+        "sales.orders",
+        "sales.order_items",
+        "production.stocks"
+      )
+    end
+
+    it "should contain all columns" do
+      @returned_array = []
+      @result["entities"].each do |obj|
+        obj[:columns].each {|column| @returned_array << column["name"]}
+      end
+      expect(@returned_array).to contain_exactly(column_list)
+    end
+
+    it "should contain the correct key mappings" do
+    end
+    
   end
 end
