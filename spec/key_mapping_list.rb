e768991653fd0f72e62d37f7e7f794dbd83d54ce
rory <rory_bell_88@hotmail.com>
2020-09-02 11:49:33 +1000

    key mapping data array added for testing purposes

--- spec/key_mapping_list.rb
+++ spec/key_mapping_list.rb
@@ -1,6 +1,54 @@
 def key_mapping_list
 return array = [
-    [{:column_name => "customerNumber", :type => "primary"}, {:column_name => "salesRepEmployeeNumber", :type => "foreign"}],
-    []
+    { :entity => "customers",
+      :keys => [
+          {:column_name => "customerNumber", :type => "primary"},
+          {:column_name => "salesRepEmployeeNumber", :type => "foreign", :ref_table => "employees", :ref_col => "employeeNumber" }
+        ]
+    },
+    { :entity => "employees",
+      :keys => [
+          {:column_name => "employeeNumber", :type => "primary"},
+          {:column_name => "reportsTo", :type => "foreign", :ref_table => "employees", :ref_col => "employeeNumber"},
+          {:column_name => "officeCode", :type => "foreign", :ref_table => "offices", :ref_col => "officeCode"}
+        ]
+    },
+    { :entity => "offices",
+      :keys => [
+          {:column_name => "officeCode", :type => "primary"}
+        ]
+    },
+    { :entity => "orderdetails",
+      :keys => [
+          {:column_name => "orderNumber", :type => "primary"},
+          {:column_name => "productCode", :type => "primary"},
+          {:column_name => "orderNumber", :type => "foreign", :ref_table => "orders", :ref_col => "orderNumber"},
+          {:column_name => "productCode", :type => "foreign", :ref_table => "products", :ref_col => "productCode"},
+        ]
+    },
+    { :entity => "orders",
+      :keys => [
+          {:column_name => "orderNumber", :type => "primary"},
+          {:column_name => "customerNumber", :type => "foreign", :ref_table => "customers", :ref_col => "customerNumber"}
+        ]
+    },
+    { :entity => "payments",
+      :keys => [
+          {:column_name => "customerNumber", :type => "primary"},
+          {:column_name => "checkNumber", :type => "primary"},
+          {:column_name => "customerNumber", :type => "foreign", :ref_table => "customers", :ref_col => "customerNumber"}
+        ]
+    },
+    { :entity => "productlines",
+      :keys => [
+          {:column_name => "productLine", :type => "primary"}
+        ]
+    },
+    { :entity => "products",
+      :keys => [
+          {:column_name => "productCode", :type => "primary"},
+          {:column_name => "productLine", :type => "foreign", :ref_table => "productlines", :ref_col => "productLine"}
+        ]
+    }
 ]
 end
