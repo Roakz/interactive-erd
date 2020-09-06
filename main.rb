942488292112549e74e0c9e2f033b1d2e96197ea
rory <rory_bell_88@hotmail.com>
2020-09-06 18:44:51 +1000

    added an erb/bootstrap frontend.

--- main.rb
+++ main.rb
@@ -6,14 +6,24 @@ require_relative 'mapper.rb'
 
 set :logger, Logger.new(STDOUT)
 
-post '/file-to-json' do
+get '/' do
+  erb :home, :layout => :layout
+end
 
-  mapper = Mapper.new 
-  mapper.load_file(params['file'][:tempfile])
-  top_level = mapper.calculate_top_level
-  entities = mapper.split_entities
-  mapper.entities_to_json(entities).to_json
- 
+get '/file-to-json-form' do
+  erb :upload_form, :layout => :layout
+end
+
+post '/file-to-json' do
+  begin
+    mapper = Mapper.new 
+    mapper.load_file(params['file'][:tempfile])
+    top_level = mapper.calculate_top_level
+    entities = mapper.split_entities
+    return [200, mapper.entities_to_json(entities).to_json]
+  rescue 
+    return [500, {:error => "something went wrong"}.to_json]
+  end 
 end
 
 
