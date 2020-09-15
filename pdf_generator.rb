14e9928e6c7bd5f36ad57c7a34d6771ae44437b7
rory <rory_bell_88@hotmail.com>
2020-09-15 11:44:21 +1000

    basic contents and tables for each entity are rendering in the pdf

--- pdf_generator.rb
+++ pdf_generator.rb
@@ -1,5 +1,6 @@
 require 'prawn'
 require 'prawn/table'
+require 'json'
 
 class PdfGenerator
 
@@ -8,29 +9,52 @@ class PdfGenerator
   def initialize(json, logger)
     @logger = logger 
     @json = json
+    @hash = JSON.parse(@json)
   end
+
 
   def generate
     @pdf = Prawn::Document.new
-    generate_heading(@pdf, "Entity Name")
-    pdf.move_down 50
-    # generate_entity_table(@pdf)
+    # take care of the top level stuff before we iterate.
+    generate_heading(@pdf, "#{@hash["top_level"]["database"].first} Interactive ERD")
+    @pdf.move_down 10
+   
+    @hash["entities"].each do |entity|
+    generate_contents_line(@pdf, entity["table_name"])
+    end
+    # @logger.error(@hash)
+    @hash["entities"].each do |entity|
+     generate_entity_tables(@pdf, entity["columns"], entity["table_name"])
+    end
     return @pdf
+  end
+
+  def generate_contents_line(pdf, table_name)
+    pdf.text table_name
+    pdf.move_down 10
+    pdf.stroke_horizontal_rule
+    pdf.move_down 10
   end
 
   def generate_heading(pdf, heading)
     pdf.move_down 50
     pdf.formatted_text_box([
     { :text => heading,
-      :size => 50,
+      :size => 25,
       :styles => [:bold] }])
     pdf.stroke_horizontal_rule
   end
 
-  def generate_entity_table(pdf)
-    pdf.table([["Header1", "Header2"],
-                  ["row1 col", "row1 col"],
-                  ["row2 col", "row2 col"]])
+  def generate_entity_tables(pdf, entity_columns, table_name)
+    table = []
+    table << ["Column", "Data type"]
+     entity_columns.each do |column|
+       table << [column["column_name"], column["data_type"]]
+     end
+     pdf.start_new_page
+     pdf.text(table_name)
+     pdf.move_down 10
+     pdf.table(table)
   end
 
 end
