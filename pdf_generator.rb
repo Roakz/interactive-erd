6fb7363894e0a867114c6d7d2779a285c1ff56f5
rory <rory_bell_88@hotmail.com>
2020-09-20 12:18:06 +1000

    links to foreign key tables are in the contents and under each table

--- pdf_generator.rb
+++ pdf_generator.rb
@@ -13,24 +13,31 @@ class PdfGenerator
   end
 
   def generate
+    @logger.error(@hash)
+
     @pdf = Prawn::Document.new
-    # take care of the top level stuff before we iterate.
+  
+    pdf.add_dest "top-link", pdf.dest_xyz(pdf.bounds.absolute_left, pdf.y)
+
     generate_heading(@pdf, "#{@hash["top_level"]["database"].first} Interactive ERD")
     @pdf.move_down 10
    
     @hash["entities"].each do |entity|
-    generate_contents_line(@pdf, entity["table_name"])
+    generate_contents_line(@pdf, entity)
     end
 
     @hash["entities"].each do |entity|
-     generate_entity_tables(@pdf, entity["columns"], entity["table_name"])
+     generate_entity_tables(@pdf, entity)
     end
     return @pdf
   end
 
-  def generate_contents_line(pdf, table_name)
-    pdf.text "<link anchor=\"#{table_name}\">#{table_name}</link>", inline_format: true
+  def generate_contents_line(pdf, entity)
+    pdf.text "<link anchor=\"#{entity["table_name"]}\"><strong>#{entity["table_name"]}</strong></link>", inline_format: true
     pdf.move_down 10
+
+    include_key_links(entity)
+
     pdf.stroke_horizontal_rule
     pdf.move_down 10
   end
@@ -44,16 +51,35 @@ class PdfGenerator
     pdf.stroke_horizontal_rule
   end
 
-  def generate_entity_tables(pdf, entity_columns, table_name)
+  def generate_entity_tables(pdf, entity)
     table = []
     table << ["Column", "Data type"]
-     entity_columns.each do |column|
-       table << [column["column_name"], column["data_type"]]
-     end
-     pdf.start_new_page
-     pdf.add_dest "#{table_name}", pdf.dest_xyz(pdf.bounds.absolute_left, pdf.y)
-     pdf.text(table_name)
-     pdf.move_down 10
-     pdf.table(table)
+
+    entity["columns"].each do |column|
+      table << [column["column_name"], column["data_type"]]
+    end
+
+    pdf.start_new_page
+    pdf.add_dest "#{entity["table_name"]}", pdf.dest_xyz(pdf.bounds.absolute_left, pdf.y)
+    pdf.text "<strong>#{entity["table_name"]}</strong>", inline_format: true
+    pdf.move_down 10
+    pdf.table(table)
+    pdf.move_down 10
+
+    include_key_links(entity)
+
+    pdf.move_down 10
+    pdf.text "<link anchor=\"top-link\"><strong>Back to top</strong></link>", inline_format: true
   end
+
+  def include_key_links(entity)
+    if entity["keys"] && entity["keys"].empty? == false
+      entity["keys"].each do |key_obj|
+        if key_obj["type"] == "foreign"
+          pdf.text "<link anchor=\"#{key_obj["ref_table"]}\">  - #{key_obj["ref_table"]}</link>", inline_format: true
+        end
+      end
+    end
+  end
+
 end
